e2d0abcea626c896a4555b2a4decfb3cd051c786
Ziad <ziad1991@hotmail.co.uk>
2020-02-06 16:01:37 +0000

    refactor global player names

--- app.rb
+++ app.rb
@@ -11,20 +11,20 @@ class Battle < Sinatra::Base
   end
 
   post '/names' do
-    $player_1_name = Player.new(params[:player_1_name])
-    $player_2_name = Player.new(params[:player_2_name])
+    $player_1 = Player.new(params[:player_1_name])
+    $player_2 = Player.new(params[:player_2_name])
     redirect '/play'
   end
   
   get '/play' do
-    @player_1_name = $player_1_name.name
-    @player_2_name = $player_2_name.name
+    @player_1_name = $player_1.name
+    @player_2_name = $player_2.name
     erb :play
   end
 
   get '/attack' do
-    @player_1_name = $player_1_name.name
-    @player_2_name = $player_2_name.name
+    @player_1_name = $player_1.name
+    @player_2_name = $player_2.name
     erb :attack
   end
 
