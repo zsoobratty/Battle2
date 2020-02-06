e64c06c7c8b1b1bcb49884227007e0d3e7b95112
Student <emilywright212@gmail.com>
2020-02-06 14:24:04 +0000

    Post redirect get pattern created

--- app.rb
+++ app.rb
@@ -1,15 +1,23 @@
 require "sinatra/base"
 
 class Battle < Sinatra::Base
-  set :session_secret, "nah nah nah"
+  # set :session_secret, "nah nah nah"
+
+  enable :sessions
 
   get '/' do
     erb :index
   end
 
   post '/names' do
-    @player_1_name = params[:player_1_name]
-    @player_2_name = params[:player_2_name]
+    session[:player_1_name] = params[:player_1_name]
+    session[:player_2_name] = params[:player_2_name]
+    redirect '/play'
+  end
+  
+  get '/play' do
+    @player_1_name = session[:player_1_name]
+    @player_2_name = session[:player_2_name]
     erb :play
   end
 
