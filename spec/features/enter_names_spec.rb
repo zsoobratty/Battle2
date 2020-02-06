15a535f6e8888ad61826b60fcf0176aace00c5c6
Ziad <ziad1991@hotmail.co.uk>
2020-02-06 14:40:58 +0000

    sign_in_and_play method for web_helper

--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -2,10 +2,7 @@ require './app'
 
 feature 'Enter names' do
   scenario 'submitting names' do
-    visit('/')
-    fill_in :player_1_name, with: 'CapyBary'
-    fill_in :player_2_name, with: 'CapyBob'
-    click_button 'Submit'
+    sign_in_and_play
     expect(page).to have_content 'CapyBary vs. CapyBob'
   end
 end
--- spec/features/hit_points_spec.rb
+++ spec/features/hit_points_spec.rb
@@ -2,10 +2,7 @@ require './app'
 
 feature "Show P2's hit points" do
   scenario 'show hit points' do
-    visit('/')
-    fill_in :player_1_name, with: 'CapyBary'
-    fill_in :player_2_name, with: 'CapyBob'
-    click_button 'Submit'
+    sign_in_and_play
     expect(page).to have_content 'CapyBob has 200 HP'
   end
 end
